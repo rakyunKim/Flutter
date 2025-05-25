a73ea075cafb716b04d49e9f5c137972181536d6
rakyun1 <dbskim234@naver.com>
2025-05-25 13:08:10 +0900

    refactor design : calcurator

--- caculator/lib/main.dart
+++ caculator/lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.deepOrange
-      ),
       home: Calculator(),
     );
   }
